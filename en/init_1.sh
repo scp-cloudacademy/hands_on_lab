4a470c8930564034479cc25dc03ccb6066d989fc
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2024-01-23 09:14:17 +0900

    Update init_1.sh

--- en/init_1.sh
+++ en/init_1.sh
@@ -9,4 +9,4 @@ sudo yum install httpd -y
 # Overwrite index.html
 sudo echo "<html><body><p>Welcome to Web Server IP: $(ip addr show | awk '/inet / {print $2}' | cut -d'/' -f1), Name: $(hostname)</p></body></html>" | sudo tee /var/www/html/index.html
 
-# echo "Complete init1 commands successfully"
+echo "Complete init1 commands successfully"
