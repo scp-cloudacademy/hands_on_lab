bef54a30ced33ec16776eff4812377a77d55fccc
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2024-01-23 09:10:54 +0900

    Update and rename init_script.sh to init_1.sh

---
+++ en/init_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update the package manager and install yum-utils
+sudo yum install yum-utils -y
+
+# Install httpd service
+sudo yum install httpd -y
+
+# Overwrite index.html
+
+sudo echo "<html><body><p>Welcome to Web Server IP: $(ip addr show | awk '/inet / {print $2}' | cut -d'/' -f1), Name: $(hostname)</p></body></html>" | sudo tee /var/www/html/index.html
