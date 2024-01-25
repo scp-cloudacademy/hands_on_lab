7cd5429fb62937353fcbd4cb8d8f92e224f58bf6
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2024-01-25 13:06:38 +0900

    Update init_1.sh

--- en/init_1.sh
+++ en/init_1.sh
@@ -6,7 +6,10 @@ sudo yum install yum-utils -y
 # Install httpd service
 sudo yum install httpd -y
 
-# Overwrite index.html
-sudo echo "<html><body><p>Welcome to Web Server IP: $(ip addr show | awk '/inet / {print $2}' | cut -d'/' -f1), Name: $(hostname)</p></body></html>" | sudo tee /var/www/html/index.html
+# Enable HTTPD
+sudo systemctl enable httpd
+
+# Start HTTPD
+sudo systemctl start httpd
 
 echo "Complete init1 commands successfully"
