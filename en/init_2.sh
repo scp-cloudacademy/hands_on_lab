9e7e7cc9ef323ac5ea10857bc1d597e0c1ff5208
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2024-01-25 13:19:01 +0900

    Update init_2.sh

--- en/init_2.sh
+++ en/init_2.sh
@@ -4,7 +4,7 @@
 sudo rm /var/html/index.html
 
 # Creating index.html for Server Information
-sudo echo "<html><body><p>Welcome to Web Server IP: $(ip addr show | awk '/inet / {print $2}' | cut -d'/' -f1), Name: $(hostname)</p></body></html>" | sudo tee /var/www/html/index.html
+sudo echo "<html><body><p style="font-size:30px">Welcome to Web Server $(hostname), IP: $(ip addr show | awk '/inet / {print $2}' | cut -d'/' -f1)</p></body></html>" | sudo tee /var/www/html/index.html
 
 # Start HTTPD
 sudo systemctl restart httpd
