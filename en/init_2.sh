f8cf64640fcb987a35fcb58b8bcad4edef56b3e9
scp-cloudacademy <147478897+scp-cloudacademy@users.noreply.github.com>
2024-01-25 13:07:08 +0900

    Update init_2.sh

--- en/init_2.sh
+++ en/init_2.sh
@@ -3,10 +3,7 @@
 # Creating index.html for Server Information
 sudo echo "<html><body><p>Welcome to Web Server IP: $(ip addr show | awk '/inet / {print $2}' | cut -d'/' -f1), Name: $(hostname)</p></body></html>" | sudo tee /var/www/html/index.html
 
-# Enable HTTPD
-sudo systemctl enable httpd
-
 # Start HTTPD
-sudo systemctl start httpd
+sudo systemctl restart httpd
 
 echo "Complete init1 commands successfully"
